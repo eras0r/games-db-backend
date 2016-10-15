629c643cbfb1ff90ebcca51cccdb54d1031d9b36
Steven Heller <er4s0r@gmail.com>
2016-10-15 21:55:09 +0200

    remove unwanted methods from the paint-color REST API endpoint

--- server/models/paint-color.js
+++ server/models/paint-color.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function(Paintcolor) {
+module.exports = function (Paintcolor) {
+
+  Paintcolor.disableRemoteMethod("upsert", true);
+  Paintcolor.disableRemoteMethod("updateAll", true);
+  Paintcolor.disableRemoteMethod("findOne", true);
+  Paintcolor.disableRemoteMethod("replaceById", true);
+  Paintcolor.disableRemoteMethod("replaceOrCreate", true);
+  Paintcolor.disableRemoteMethod("createChangeStream", true);
+  Paintcolor.disableRemoteMethod("upsertWithWhere", true);
+  Paintcolor.disableRemoteMethod("confirm", true);
+  Paintcolor.disableRemoteMethod("count", true);
+  Paintcolor.disableRemoteMethod("exists", true);
 
 };

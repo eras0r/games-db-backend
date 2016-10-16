41add81d37fb2262608ab560be9a06f0a5c11836
Steven Heller <er4s0r@gmail.com>
2016-10-16 02:37:59 +0200

    add boot script to create roles

---
+++ server/boot/10-insert-roles.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var log = require('./../middleware/logger');
+
+module.exports = function (app, callback) {
+
+  log.info('--- boot script "10-insert-roles" started... ---- ');
+
+  var Role = app.models.Role;
+
+  app.dataSources.mongoDb.autoupdate('Role', function (err) {
+    if (err) {
+      throw err;
+    }
+
+    // check Roles
+    Role.count(function (err, roleCount) {
+      log.debug('Number of Roles: %d', roleCount);
+      // we have no roles yet
+      if (roleCount <= 0) {
+        log.info('No Roles are present yet. Insert Role master data...');
+        insertRoles();
+      }
+      else {
+        log.info('Roles are already present. No need to insert Role master data.');
+      }
+    });
+
+  });
+
+  /**
+   * Inserts the roles master data.
+   */
+  function insertRoles() {
+    Role.create([
+      {name: 'ADMIN'},
+      {name: 'GAMER'},
+      {name: 'PAINTER'}
+    ], function (err, roles) {
+      if (err) {
+        throw err;
+      }
+
+      log.info('Number of Roles created: %d', Object.keys(roles).length);
+
+      log.info('--- boot script "10-insert-roles"  ended ---- ');
+      // invoke async bootscript callback function
+      callback();
+    });
+  }
+
+};
